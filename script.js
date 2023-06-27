5bc5f3c2348bf7f4749d55694de490a423bd498c
PouePouePoue <137453257+PouePouePoue@users.noreply.github.com>
2023-06-27 18:32:54 +0300

    LEsson 2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+function checkPalindrome(str) {
+
+    const len = str.length;
+
+    for (let i = 0; i < len / 2; i++) {
+
+        if (str[i] !== str[len - 1 - i]) {
+            return 'False';
+        }
+    }
+    return 'True';
+}
+const str = prompt('Enter a word: ');
+const value = checkPalindrome(str);
+
+console.log(value);
