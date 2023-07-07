052f3fe85a1bdde2f7b2d029433f2aa3e57f9cb4
PouePouePoue <137453257+PouePouePoue@users.noreply.github.com>
2023-07-07 23:35:18 +0300

    Add files via upload

---
+++ 2/scripts.js
@@ -0,0 +1,47 @@
+const a = 5;
+const b = 7;
+ if (a>b){
+    console.log(a);
+}else{
+    console.log(b);
+}
+
+const num=prompt('Enter a number 1-12');
+switch (num){
+    case '1':
+        console.log("Январь");
+        break;
+    case '2':
+        console.log("Февраль");
+        break;
+    case '3':
+        console.log("Март");
+        break;
+    case '4':
+        console.log("Апрель");
+        break;
+    case '5':
+        console.log("Май");
+        break;
+    case '6':
+        console.log("Июнь");
+        break;
+    case '7':
+        console.log("Июль");
+        break;
+    case '8':
+        console.log("Август");
+        break;
+    case '9':
+        console.log("Сентябрь");
+        break;
+    case '10':
+        console.log("Октябрь");
+        break;
+    case '11':
+        console.log("Ноябрь");
+        break;
+    case '12':
+        console.log("Декабрь");
+        break;
+}
